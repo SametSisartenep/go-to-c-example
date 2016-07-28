d35b7dee6ebf397b0feca08063e478e7d0f60c1b
SametSisartenep <rodrigosloop@gmail.com>
2016-07-29 00:43:51 +0200

    C syntax style updated.

--- hi.c
+++ hi.c
@@ -5,23 +5,22 @@
 
 void usage(char *command)
 {
-  fprintf(stderr, "\t~$ %s <name>\n\n", command);
+	fprintf(stderr, "\t~$ %s <name>\n\n", command);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
-  {
-    fprintf(stderr, "Not enough arguments.\n\n");
-    usage(argv[0]);
-    exit(1);
-  }
+	if (argc != 2) {
+		fprintf(stderr, "Not enough arguments.\n\n");
+		usage(argv[0]);
+		exit(1);
+	}
 
-  GoString name = { argv[1], strlen(argv[1]) };
+	GoString name = { argv[1], strlen(argv[1]) };
 
-  say_hello(name);
+	say_hello(name);
 
-  say_bye();
+	say_bye();
 
-  return 0;
+	return 0;
 }
